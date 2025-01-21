f49549cc79adc79a97cfe9ffa2d6dc2c3fbf0c70
tanay388 <tanay.deo388@gmail.com>
2025-01-21 12:24:15 +0530

    added payment confirmation for stripe

--- src/models/wallet/wallet.module.ts
+++ src/models/wallet/wallet.module.ts
@@ -3,8 +3,10 @@ import { WalletService } from './wallet.service';
 import { WalletController } from './wallet.controller';
 import { TransactionsService } from '../transactions/transactions.service';
 import { PaymentGatewayService } from './services/payment-gateway.service';
+import { EmailModule } from 'src/providers/email/email.module';
 
 @Module({
+  imports: [EmailModule],
   controllers: [WalletController],
   providers: [WalletService, TransactionsService, PaymentGatewayService],
 })
