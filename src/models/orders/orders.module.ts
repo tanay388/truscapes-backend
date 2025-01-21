d6276009804f5486fe36d284520996ce5e7282a6
tanay388 <tanay.deo388@gmail.com>
2025-01-21 12:42:36 +0530

    added payment confirmation for stripe

--- src/models/orders/orders.module.ts
+++ src/models/orders/orders.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { PaymentGatewayService } from '../wallet/services/payment-gateway.service';
+import { EmailModule } from 'src/providers/email/email.module';
 
 @Module({
+  imports: [EmailModule],
   controllers: [OrdersController],
   providers: [OrdersService, PaymentGatewayService],
 })
